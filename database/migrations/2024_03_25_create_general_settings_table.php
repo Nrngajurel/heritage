51eb09579bc8f8678db63b944b458329ffab3f92
Narayan Gajurel <nrngajurel@Narayans-MacBook-Pro.local>
2025-03-30 08:22:41 +0545

    c

--- database/migrations/2024_03_25_create_general_settings_table.php
+++ database/migrations/2024_03_25_create_general_settings_table.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
-use Spatie\LaravelSettings\Migrations\SettingsMigration;
 
-return new class extends SettingsMigration
+return new class
 {
     public function up(): void
     {
